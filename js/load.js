819fde48493beabeabc2724073b71ab2dfef23dd
Martin0828 <945144380@qq.com>
2016-12-01 14:24:54 +0800

    Create load.js

---
+++ js/load.js
@@ -0,0 +1,21 @@
+define(function(require,exports,module){
+	function load(){
+		var oPic=document.getElementById("pic");
+		var oPic2=document.getElementById("pic2");
+		var oPic3=document.getElementById("pic3");
+		var oShadow=document.getElementById("shadow");
+		var oText=document.getElementById("text");
+		window.location.hash="message";
+		var num=0;
+		var timer=setInterval(add,10);
+		function add(){
+			num++;
+			if (num==100) {
+				clearInterval(timer);
+				require("show.js").show();
+			}
+			oText.innerHTML=num+"%";
+		}
+	}
+	load();
+})
