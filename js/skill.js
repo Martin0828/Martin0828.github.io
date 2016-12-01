965d91dcc48838699b523eb2a8d104542b408f1d
Martin0828 <945144380@qq.com>
2016-12-01 14:28:09 +0800

    Create skill.js

---
+++ js/skill.js
@@ -0,0 +1,13 @@
+define(function(require,exports,module){
+	var aSkill=document.getElementsByClassName("ability");
+	function initIn(i){
+		require("startmove.js").startMove(aSkill[i-1],{"opacity":100});
+	}
+	function initOut(){
+		for(var i=0;i<aSkill.length;i++){
+			aSkill[i].style.opacity=0;
+		}
+	}
+	exports.initIn=initIn;
+	exports.initOut=initOut;
+})
