4231d45e8e18891bd23457375351a471efdddbb1
Martin0828 <945144380@qq.com>
2016-12-08 21:18:10 +0800

    Update skill.js

--- js/skill.js
+++ js/skill.js
@@ -1,13 +1,17 @@
-define(function(require,exports,module){
-	var aSkill=document.getElementsByClassName("ability");
-	function initIn(i){
-		require("startmove.js").startMove(aSkill[i-1],{"opacity":100});
+define(function( require, exports, module ){
+	var aSkill = document.getElementsByClassName("ability");
+	function initIn( i ){
+		require("startmove.js").startMove( aSkill[i-1],{"opacity":100});
 	}
 	function initOut(){
-		for(var i=0;i<aSkill.length;i++){
-			aSkill[i].style.opacity=0;
+		for( var i = 0; i < aSkill.length; i++ ){
+			aSkill[i].style.opacity = 0;
 		}
 	}
-	exports.initIn=initIn;
-	exports.initOut=initOut;
+	exports.initIn = initIn;
+	exports.initOut = initOut;
+// <<<<<<< HEAD
 })
+// =======
+// })
+// >>>>>>> origin/master
