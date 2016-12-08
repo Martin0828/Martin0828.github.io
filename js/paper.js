fa95b5534dd84ea11da3d6a4e686bead767251fb
Martin0828 <945144380@qq.com>
2016-12-08 21:19:30 +0800

    Update paper.js

--- js/paper.js
+++ js/paper.js
@@ -1,21 +1,30 @@
-define(function(require,exports,module){
+define(function( require, exports, module ){
 	function paper(){
-		var oWrap=document.getElementsByClassName("wrap")[0];
-		var aDiv=oWrap.getElementsByTagName("div");
-		var oTimer=null;
-		var i=0;
+
+		var oWrap = document.getElementsByClassName("wrap")[0];
+		var aDiv = oWrap.getElementsByTagName("div");
+		var oTimer = null;
+		var i = 0;
 		open();
+
 		function open(){
 			clearInterval(oTimer);
-			i=0;
-			oTimer=setInterval(function(){
-				aDiv[i].className="show";
+			i = 0;
+
+			oTimer = setInterval(function(){
+				aDiv[i].className = "show";
 				i++;
-				if(i==aDiv.length){
+				if( i == aDiv.length){
 					clearInterval(oTimer);
 				}
-			},100)
+			}, 100)
+			
 		}
 	}
-	exports.paper=paper;
+
+	exports.paper = paper;
+// <<<<<<< HEAD
 })
+// =======
+// })
+// >>>>>>> origin/master
