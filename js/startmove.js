cbcf5e9e1a0bc829f26adb442ac5f458b0d7a89a
Martin0828 <945144380@qq.com>
2016-12-08 21:14:05 +0800

    Update startmove.js

--- js/startmove.js
+++ js/startmove.js
@@ -1,40 +1,55 @@
-define(function(require,exports,module){
-	function startMove(obj,json,endFn ) {
-		clearInterval(obj.timer);
-		obj.timer = setInterval(function () {
+define( function( require, exports, module){
+
+	function startMove( obj, json, endFn ) {
+		clearInterval( obj.timer );
+
+		obj.timer = setInterval( function () {
 			var bStop = true;
+
 			for (var attr in json) {
 				var iCur = 0;
-				if (attr == 'opacity') {
-					iCur = parseInt(parseFloat(getStyle(obj, attr)) * 100);
+
+				if ( attr == 'opacity' ) {
+					iCur = parseInt( parseFloat( getStyle( obj, attr )) * 100 );
 				}
+
 				else {
-					iCur = parseInt(getStyle(obj, attr));
+					iCur = parseInt( getStyle( obj, attr ));
 				}
-				var iSpeed = (json[attr] - iCur) / 5;
-				iSpeed = iSpeed > 0 ? Math.ceil(iSpeed) : Math.floor(iSpeed);
-				if (iCur != json[attr]) {
+
+				var iSpeed = ( json[attr] - iCur ) / 5;
+				iSpeed = iSpeed > 0 ? Math.ceil( iSpeed ) : Math.floor( iSpeed );
+
+				if ( iCur != json[attr] ) {
 					bStop = false;
 				}
-				if (attr == 'opacity') {
-					obj.style.filter = 'alpha(opacity:' + (iCur + iSpeed) + ')';
-					obj.style.opacity = (iCur + iSpeed) / 100;
+
+				if ( attr == 'opacity' ) {
+					obj.style.filter = 'alpha( opacity:' + ( iCur + iSpeed ) + ')';
+					obj.style.opacity = ( iCur + iSpeed ) / 100;
 				}
+
 				else {
 					obj.style[attr] = iCur + iSpeed + 'px';
 				}
+				
 			}
-			if (bStop) {
-				clearInterval(obj.timer);
-				if (endFn) {
+
+			if ( bStop ) {
+				clearInterval( obj.timer );
+				if ( endFn ) {
 					endFn();
 				}
 			}
-		}, 10);
+		}, 30 );
+
 	}
-	exports.startMove=startMove;
+
+	exports.startMove = startMove;
+
 	function getStyle( obj, attr ){
 		return obj.currentStyle ? obj.currentStyle[attr] : getComputedStyle( obj )[attr];
 	}
+
 })
 
